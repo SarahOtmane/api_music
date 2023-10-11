ca9e0bb5b1b6470a4a0bf7750666c3c9823265ae
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2023-10-11 16:20:39 +0200

    creat the fuction : listAllMusics and its route

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,1 +1,14 @@
-const Music = require('../models/postModel');
+const Music = require('../models/musicModel');
+
+exports.listAllMusics = async (req, res) =>{
+    try{
+        const musics = await Music.find({});
+        res.status(200);
+        res.json(musics);
+    }
+    catch(error){
+        res.status(500);
+            console.log(error);
+            res.json({ message : 'Erreur serveur'});
+    }
+}
