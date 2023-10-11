d8598553e67d319071df388cafb18a26dc5bec87
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2023-10-11 16:13:24 +0200

    initialize express in musicRoute, mongoose in musicModel and create musicController

---
+++ routes/musicRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router  = express.Router(); 
+
+const musicController = require('../controllers/musicController');
+
+
+// router
+//     .route('/musics')
+//     .get(postController.listAllPosts)
+//     .post(postController.createAPost)
+
+
+// router 
+//     .route('/musics/:id_music')
+//     .get(postController.getAPost)
+//     .put(postController.updateAPost)
+//     .delete(postController.deleteAPost)
+
+module.exports = router;
